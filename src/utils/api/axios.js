a0ad7421bc44307171b1ad000a41ef7899cd65b7
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2024-04-17 02:05:12 -0600

    Update axios.js

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL:'api.labarbada.store/api',
-  baseURL:'http://localhost:3000/api',
-  //baseURL:'https://api-barbada.vercel.app/api',
+  //baseURL:'http://localhost:3000/api',
+  baseURL:'https://api-barbada.vercel.app/api',
   withCredentials: true
 })
 
